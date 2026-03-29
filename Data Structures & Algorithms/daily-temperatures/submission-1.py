af840295b1c64e7fbc636a99cd5bec6a7400d63d
Sergei Borja <76608956+wonderlust101@users.noreply.github.com>
2026-03-29 09:59:42 -0600

    Add: daily-temperatures - submission-1

---
+++ Data Structures & Algorithms/daily-temperatures/submission-1.py
@@ -0,0 +1,13 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        res = []
+        for i in range(len(temperatures)):
+            highestPos = 0
+            for j in range(i, len(temperatures)):
+                if temperatures[i] < temperatures[j]:
+                    highestPos = j
+                    break
+            
+            res.append(highestPos - i if highestPos > i else 0)
+        
+        return res
