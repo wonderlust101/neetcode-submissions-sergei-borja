20a2a4ff94592f88445f209da8b307a54cc4e42a
Sergei Borja <76608956+wonderlust101@users.noreply.github.com>
2026-03-31 17:07:53 -0600

    Add: is-anagram - submission-9

---
+++ Data Structures & Algorithms/is-anagram/submission-9.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        alpha = [0] * 26
+
+        for l in s:
+            alpha[ord(l) - ord("a")] += 1
+
+        for l in t:
+            alpha[ord(l) - ord("a")] -= 1
+
+        for i in alpha:
+            if i != 0:
+                return False
+        
+        return True
+        
