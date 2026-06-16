27d096b65bf53e578cc47634914bc880bcd4e048
Sergei Borja <sergei.borja0107@gmail.com>
2026-06-15 20:48:50 -0600

    Add: sql-common-table-expressions - submission-6

---
+++ SQL/sql-common-table-expressions/submission-6.sql
@@ -0,0 +1,53 @@
+CREATE TABLE customers (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  name TEXT
+);
+
+CREATE TABLE orders (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  customer_id INTEGER,
+  price INTEGER
+);
+
+INSERT INTO customers (name) VALUES
+  ('Alice'),
+  ('Bob'),
+  ('Charlie'),
+  ('David'),
+  ('Eve'),
+  ('Frank'),
+  ('Grace'),
+  ('Hank');
+
+INSERT INTO orders (customer_id, price) VALUES
+  (1, 50),
+  (2, 100),
+  (3, 150),
+  (4, 200),
+  (5, 250),
+  (6, 30),
+  (7, 70),
+  (8, 400);
+-- Do not modify above this line. --
+
+
+WITH max_100_order AS (
+    SELECT customer_id, price
+    FROM orders
+    WHERE price < 100
+)
+
+SELECT name
+FROM customers
+JOIN max_100_order o ON o.customer_id = id
+ORDER BY name;
+
+
+
+
+
+
+
+
+
+
